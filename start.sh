f2d7f4597338a265ad26f5a59a1c7beb0f9feab3
Firman Yudistia <firman.yudistia@codemi.co.id>
2026-04-06 12:14:49 +0700

    remove key:generate from startup script

--- start.sh
+++ start.sh
@@ -8,9 +8,6 @@ PORT=${PORT:-8080}
 sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
 sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-available/000-default.conf
 
-# Generate app key if not set
-php artisan key:generate --force
-
 # Cache config, routes, views
 php artisan config:cache
 php artisan route:cache
